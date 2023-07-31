161b75733b7511f90634536bd86bf4cd751004ea
impronen <impronen@gmail.com>
2023-07-31 18:44:29 +0300

    remove useEffect from Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,54 +1,45 @@
 import React, { useState } from "react";
 
 export default function Experience({ updateExperienceData }) {
-  const [company, setCompany] = useState("");
-  const [position, setPosition] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [onGoing, setOnGoing] = useState("");
-  const [jobs, setJobs] = useState([]);
+  const [state, setState] = useState({
+    company: "",
+    position: "",
+    startDate: "",
+    endDate: "",
+    onGoing: false,
+    jobs: [],
+  });
 
-  // Basic editing of job properties
-  function changeCompany(e) {
-    setCompany(e.target.value);
-  }
-
-  function changePosition(e) {
-    setPosition(e.target.value);
-  }
-
-  function changeStartDate(e) {
-    setStartDate(e.target.value);
-  }
-
-  function changeEndDate(e) {
-    setEndDate(e.target.value);
-  }
-
-  function changeOnGoing(e) {
-    setOnGoing(e.target.checked);
+  function handleChange(e) {
+    const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: value,
+    });
   }
 
   function addJob(e) {
     e.preventDefault();
 
     const newJob = {
-      company: company,
-      position: position,
-      startDate: startDate,
-      endDate: endDate,
-      onGoing: onGoing,
+      company: state.company,
+      position: state.position,
+      startDate: state.startDate,
+      endDate: state.endDate,
+      onGoing: state.onGoing,
     };
 
-    setJobs((prevJobs) => [...prevJobs, newJob]);
+    setState((prevState) => ({
+      ...prevState,
+      jobs: [...prevState.jobs, newJob],
+      company: "",
+      position: "",
+      startDate: "",
+      endDate: "",
+      onGoing: false,
+    }));
 
     updateExperienceData((prevJobs) => [...prevJobs, newJob]);
-
-    setCompany("");
-    setPosition("");
-    setStartDate("");
-    setEndDate("");
-    setOnGoing("");
   }
 
   return (
@@ -58,29 +49,48 @@ export default function Experience({ updateExperienceData }) {
         <div>
           <div className="formElement nameElement fullWidthInput">
             <label>Company</label>
-            <input value={company} onChange={changeCompany} />
+            <input
+              name="company"
+              value={state.company}
+              onChange={handleChange}
+            />
           </div>
           <div className="formElement nameElement fullWidthInput">
             <label>Position</label>
-            <input value={position} onChange={changePosition} />
+            <input
+              name="position"
+              value={state.position}
+              onChange={handleChange}
+            />
           </div>
 
           <div className="formElement dateRow fullWidthInput">
             <label className="dateLabel">Start Date</label>
 
-            <input value={startDate} onChange={changeStartDate} type="month" />
+            <input
+              name="startDate"
+              value={state.startDate}
+              onChange={handleChange}
+              type="month"
+            />
           </div>
           <div className="formElement dateRow fullWidthInput">
             <label className="dateLabel">End Date</label>
-            <input value={endDate} onChange={changeEndDate} type="month" />
+            <input
+              name="endDate"
+              value={state.endDate}
+              onChange={handleChange}
+              type="month"
+            />
           </div>
           <div className="checkboxContainer">
             <label htmlFor="ongoingCheckbox">Ongoing</label>
             <input
+              name="onGoing"
               id="ongoingCheckbox"
               type="checkbox"
-              checked={onGoing}
-              onChange={changeOnGoing}
+              checked={state.onGoing}
+              onChange={handleChange}
             />
           </div>
 
